eb57e4f66f3053cdf923615e5b8de2115e5afd79
Chirath0627 <chamu0627@gmail.com>
2026-01-15 21:30:04 +0530

    Changed input from meters to centimeters

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ import streamlit as st
 st.set_page_config(page_title="BMI Calculator", page_icon="⚖️")
 
 st.title("⚖️ Health Metrics: BMI Calculator")
-st.write("Docker & CI/CD igena ganna hadapu simple project ekak.")
 
 # Input fields
 weight = st.number_input("Oyaage bara danna (kg):", min_value=1.0, step=0.1)
-height = st.number_input("Oyaage usa danna (meters):", min_value=0.1, step=0.01)
+
+
+height_cm = st.number_input("Oyaage usa danna (cm):", min_value=1.0, step=1.0)
 
 if st.button("Calculate BMI"):
-    if height > 0:
-        bmi = weight / (height ** 2)
+    if height_cm > 0:
+        
+        height_m = height_cm / 100
+        bmi = weight / (height_m ** 2)
+        
         st.subheader(f"Oyaage BMI eka: {bmi:.2f}")
         
         if bmi < 18.5:
